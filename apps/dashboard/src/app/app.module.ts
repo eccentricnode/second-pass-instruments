ef09f83f05ecf2617c280682b2afc85aa2115a07
eccentricnode <shival.bigman@gmail.com>
2019-08-15 23:34:11 -0700

    initial setup

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CoreDataModule } from '@second-pass/core-data';
+import { CoreStateModule } from '@second-pass/core-state';
+import { MaterialModule } from '@second-pass/material';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CoreDataModule,
+    CoreStateModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
